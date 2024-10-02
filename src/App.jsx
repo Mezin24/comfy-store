06052f6c9a5c5db5398f36b998c7e62923fa50f4
Pavel Mezencev <mezencev24@mail.ru>
2024-10-02 16:08:34 +0300

    add routes

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'products',
+        element: <Products />,
+      },
+      {
+        path: 'products/:id',
+        element: <SingleProduct />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+      },
+      {
+        path: 'checkout',
+        element: <Checkout />,
+      },
+      {
+        path: 'orders',
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: '/register',
+    element: <Register />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <Error />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
+export const HomeLayout = () => {
+  return (
+    <>
+      <nav>
+        <span className='text-4xl text-primary'>Comfy</span>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
